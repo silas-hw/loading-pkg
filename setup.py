55e0795cb26bf2de2cce3caece0c838929268039
Silas HW <silas.hayes.williams@gmail.com>
2021-12-09 21:50:09 +0000

    change package name... again

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="loading",
+    name="load-anim",
     version="1.0.0",
     author="Silas Hayes-Williams",
     author_email="silas.hayes.williams@gmail.com",
