dff58b16c2df7e2725f8ed2a9925676a1b811bde
Silas HW <silas.hayes.williams@gmail.com>
2021-12-09 21:36:53 +0000

    use dynamic setup file

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="loading-pkg-silas-hw",
+    version="1.0.0",
+    author="Silas Hayes-Williams",
+    author_email="silas.hayes.williams@gmail.com",
+    description="Displays cool loading animations to the terminal",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/silas-hw/loading-pkg",
+    project_urls={
+        "Bug Tracker": "https://github.com/silas-hw/loading-pkg/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
