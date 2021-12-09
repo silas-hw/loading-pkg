18a0231d3eca437b81c0a11f45a5aa188714eec4
Silas HW <silas.hayes.williams@gmail.com>
2021-12-09 21:59:06 +0000

    remove hiphen from name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="load-anim",
+    name="load_anim",
     version="1.0.1",
     author="Silas Hayes-Williams",
     author_email="silas.hayes.williams@gmail.com",
