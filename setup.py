6639613ee0c03767f10b521ccb8cc1351c52e1e3
Silas-Laptop1 <silas.hayes.williams@gmail.com>
2021-12-10 11:35:05 +0000

    fix readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
 name="loadanim",
-    version="1.0.3",
+    version="1.0.4",
     author="Silas Hayes-Williams",
     author_email="silas.hayes.williams@gmail.com",
     description="Displays cool loading animations to the terminal",
